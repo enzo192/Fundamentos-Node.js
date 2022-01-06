785bea988be2c2b6c544e9688296cfc18b8814bb
Enzo Guido <enzo192@gmail.com>
2022-01-06 11:00:09 -0500

    variables entorno

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -1,3 +1,5 @@
-let saludo = process.env.NOMBRE
+let nombre = process.env.NOMBRE || 'sin nombre';
+let web = process.env.WEB || 'no tengo web';
 
 console.log('Hola ' + nombre);
+console.log('Mi web es ' + web);
